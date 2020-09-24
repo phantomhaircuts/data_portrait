fa14d8eb347061e62f61701c369d6c9a563d890a
Erik Anderson <erik@Eriks-MBP.lan1>
2020-09-24 15:20:53 -0400

    brutalist framework

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,10 @@ function scrollFunction() {
   if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
     document.getElementById("header").style.height = "100px";
     document.querySelector("header").style.fontSize = "30px";
+    document.querySelector(".creditz").style.display = "none";
   } else {
     document.getElementById("header").style.height = "100vh";
     document.querySelector("header").style.fontSize = "100px"
+    document.querySelector(".creditz").style.display = "block";
   }
 }
